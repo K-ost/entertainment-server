05f5690376769410436b51f8f9d088ed12469b8d
K-ost <golubkovru@gmail.com>
2024-09-13 19:49:26 +0300

    adding auth

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const jsonServer = require("json-server");
+const auth = require("json-server-auth");
 
 const PORT = process.env.PORT || 8000;
 
 const server = jsonServer.create();
-
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
+server.db = router.db;
+
 server.use(middlewares);
+server.use("/api", auth);
 server.use("/api", router);
 server.listen(PORT, () => {
-  console.log(`JSON Server is running on http://localhost:${PORT}`);
+  console.log(`Server started on http://localhost:${PORT}`);
 });
